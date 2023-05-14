cdf53216b351d60bad5df2bf910441bc1a2eaa5f
prosenjitghosh <prosenjitg65@gmail.com>
2023-05-14 15:49:32 +0530

    .

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { dayOfTheWeek } from './DayFinder';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('getDay returns the long-format day of the week', () => {
+    const day = dayOfTheWeek( new Date('3/11/2020') );
+    expect( day ).toBe('Wednesday');
 });
---
+++ src/DayFinder.js
@@ -0,0 +1,5 @@
+export const dayOfTheWeek = (date = new Date()) => {
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    return days[date.getDay()];
+}
