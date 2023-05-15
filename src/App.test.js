d937ded92f97d101106e401295f28f1cac0defc5
prosenjitghosh <prosenjitg65@gmail.com>
2023-05-15 17:09:45 +0530

    testing git action flow

--- src/App.test.js
+++ src/App.test.js
@@ -9,6 +9,6 @@ import { dayOfTheWeek } from './DayFinder';
 // });
 
 test('getDay returns the long-format day of the week', () => {
-    const day = dayOfTheWeek( new Date('3/11/2020') );
+    const day = dayOfTheWeek( new Date('4/11/2020') );
     expect( day ).toBe('Wednesday');
 });
